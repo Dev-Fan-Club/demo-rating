11200b5fd794544c7a55dc785122c0e50d9e24d9
nobijoy <nobijoy1446@gmail.com>
2022-09-04 13:00:30 +0600

    ok

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-function ratingPanel(){
-    document.write("");
-}
+const mainContainer = document.querySelector(".rating")
+const thanksContainer = document.querySelector(".thanks")
+
+const submitButton = document.getElementById("submit")
+
+
+
+
+submitButton.addEventListener("click", () => {
+  
+  thanksContainer.classList.remove("hidden")
+  mainContainer.style.display = "none"
+})
