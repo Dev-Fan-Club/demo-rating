4c5ac46dfd29b9cd8468a97ff287721ee6f58cbc
“Anonna_Ferdaus” <anonnatahsin132@gmail.com>
2022-09-06 17:45:17 +0600

    update-file

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const mainContainer = document.querySelector(".container")
 const thanksContainer = document.querySelector(".thank-you")
 
-const submitButton = document.getElementById("submit")
-
+const submitButton = document.getElementById("submit-rating")
+const ratings = document.querySelectorAll(".btn")
+const actualRating = document.getElementById("rating")
 
 
 
@@ -10,4 +11,11 @@ submitButton.addEventListener("click", () => {
   
   thanksContainer.classList.remove("hidden")
   mainContainer.style.display = "none"
+  
+  ratings.forEach((rating) => {
+    rating.addEventListener("click", () => {
+      actualRating.innerHTML = rating.innerHTML
+    })
+  })
 })
+
