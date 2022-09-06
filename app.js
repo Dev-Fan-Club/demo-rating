dc4fd0e5d8290cf7f4f59dae3f7dae8faa3f5649
nobijoy <nobijoy1446@gmail.com>
2022-09-06 19:28:31 +0600

    ok

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const mainContainer = document.querySelector(".rating")
 const thanksContainer = document.querySelector(".thanks")
-
+const ratedValueElement = document.querySelector(".rates");
 const submitButton = document.getElementById("submit")
 
+let rate = 0;
 
-
+function getRating(){
+  rate = document.getElementById('btn').innerText;
+}
 
 submitButton.addEventListener("click", () => {
   
-  thanksContainer.classList.remove("hidden")
-  mainContainer.style.display = "none"
+  thanksContainer.classList.remove("hidden");
+  mainContainer.style.display = "none";
+  ratedValueElement.innerText = `You selected ${rate} out of 5`;
 })
+
